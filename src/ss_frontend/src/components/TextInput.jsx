07c275cc3224c6d1cb26a40ff736f1db7fc4eb79
TheoJustin <theojustinx@gmail.com>
2024-08-22 12:45:40 +0800

    merge

--- src/ss_frontend/src/components/TextInput.jsx
+++ src/ss_frontend/src/components/TextInput.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const TextInput = () => {
   return (
-    <div>
+    <div className='flex justify-center items-center'>
       <input
         type="text"
         placeholder="Type here"
-        className="input input-bordered w-full max-w-xs"
+        className="input input-bordered w-full max-w-xs p-8"
       />
     </div>
   );
