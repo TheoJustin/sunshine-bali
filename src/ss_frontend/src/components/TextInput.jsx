f4fb53e8c54d1ecd7418e012bcae8aa30cec39b3
TheoJustin <theojustinx@gmail.com>
2024-08-22 14:02:53 +0800

    feats/profile,input

--- src/ss_frontend/src/components/TextInput.jsx
+++ src/ss_frontend/src/components/TextInput.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React from "react";
+import { HiOutlinePaperAirplane } from "react-icons/hi2";
+import { LuImagePlus } from "react-icons/lu";
 
 const TextInput = () => {
   return (
-    <div className='flex justify-center items-center'>
-      <input
-        type="text"
-        placeholder="Type here"
-        className="input input-bordered w-full max-w-xs p-8"
-      />
-    </div>
+    <>
+        <input
+          type="text"
+          placeholder="Type here"
+          className="input input-bordered pt-6 pb-6 w-full"
+        />
+        <LuImagePlus className="text-4xl cursor-pointer" />
+        <HiOutlinePaperAirplane className="text-4xl cursor-pointer mr-8" />
+    </>
   );
-}
+};
 
-export default TextInput
+export default TextInput;
