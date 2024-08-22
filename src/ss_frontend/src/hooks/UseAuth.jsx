7c8ec61eb88c4c1e13370b918efde939e2115878
josetano2 <josejonathan.tano@gmail.com>
2024-08-22 13:04:25 +0700

    feat: internet identity & routes

---
+++ src/ss_frontend/src/hooks/UseAuth.jsx
@@ -0,0 +1,8 @@
+/**
+ * @type {React.FC}
+ */
+
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+
+export const useAuth = () => useContext(AuthContext);
--- src/ss_frontend/src/main.jsx
+++ src/ss_frontend/src/main.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import HomePage from './pages/HomePage';
-import './index.scss';
-import './tailwind.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import HomePage from "./pages/HomePage";
+import "./index.scss";
+import "./tailwind.css";
+import LoginPage from "./pages/LoginPage";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HomePage />
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 );
