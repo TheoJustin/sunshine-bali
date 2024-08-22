7a7af600fc3aeba35e126c05ed1f8246de48088d
Ryan Wantouw <wantouwr@gmail.com>
2024-08-22 14:04:45 +0800

    feats/uploadPhoto (not finished)

--- src/ss_frontend/src/main.jsx
+++ src/ss_frontend/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App';
 import HomePage from './pages/HomePage';
 import './index.scss';
 import './tailwind.css';
+import PhotoTest from './pages/PhotoTest';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
